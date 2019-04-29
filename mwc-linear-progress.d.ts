9ae97638f9cf32cbe78d03333ac4d2ef0d9d0576
Andrew Silluron <asilluron@users.noreply.github.com>
2019-04-29 11:41:39 -0700

    Initial setup

---
+++ mwc-linear-progress.d.ts
@@ -0,0 +1,41 @@
+/**
+@license
+Copyright 2018 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { BaseElement } from '@material/mwc-base/base-element.js';
+import MDCLinearProgressFoundation from '@material/linear-progress/foundation.js';
+import { MDCLinearProgressAdapter } from '@material/linear-progress/adapter.js';
+declare global {
+    interface HTMLElementTagNameMap {
+        'mwc-linear-progress': LinearProgress;
+    }
+}
+export declare class LinearProgress extends BaseElement {
+    protected mdcFoundation: MDCLinearProgressFoundation;
+    protected readonly mdcFoundationClass: typeof MDCLinearProgressFoundation;
+    protected mdcRoot: HTMLElement;
+    protected primaryBar: HTMLElement;
+    protected bufferElement: HTMLElement;
+    determinate: boolean;
+    progress: number;
+    buffer: number;
+    reverse: boolean;
+    closed: boolean;
+    static styles: import("lit-element/lib/css-tag").CSSResult;
+    render(): import("lit-html/lib/template-result").TemplateResult;
+    protected createAdapter(): MDCLinearProgressAdapter;
+    open(): void;
+    close(): void;
+}
